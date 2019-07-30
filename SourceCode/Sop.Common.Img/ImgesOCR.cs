ef7ae86dcca9903a08ad63f52341aa111e8c09f9
guojiaqiu <sopcce@qq.com>
2019-07-30 18:34:28 +0800

    add

---
+++ SourceCode/Sop.Common.Img/ImgesOCR.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tesseract;
+using System.DrawingCore;
+using Bitmap = System.Drawing.Bitmap;
+
+namespace Sop.Common.Img
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ImgesOcr
+    {
+        public string GetText(string dataPath, string filename)
+        {
+            using (var engine = new TesseractEngine(dataPath, "eng", EngineMode.Default))
+            {
+                // have to load Pix via a bitmap since Pix doesn't support loading a stream.
+                using (var image = new Bitmap(filename))
+                {
+                    using (var pix = PixConverter.ToPix(image))
+                    {
+                        using (var page = engine.Process(pix))
+                        {
+                            var text = String.Format("{0:P}", page.GetMeanConfidence());
+                            return page.GetText();
+                        }
+                    }
+                }
+
+            }
+
+        }
+    }
