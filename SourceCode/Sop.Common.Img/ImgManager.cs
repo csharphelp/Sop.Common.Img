1c99ab71514e131a1ee0f892664bba5714ea703f
guojiaqiu <sopcce@qq.com>
2019-08-13 17:20:44 +0800

    and Tesseract code

---
+++ SourceCode/Sop.Common.Img/ImgManager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DrawingCore.Imaging;
+using System.DrawingCore;
+using System.Text;
+
+namespace Sop.Common.Img
+{
+    /// <summary>
+    /// 图片高级处理接口为开发者提供了一系列高级图片处理功能，包括缩放、裁剪、旋转等。 psd、jpeg、png、gif、webp、tiff、bmp
+    /// </summary>
+    public class ImgManager
+    {
+        /// <summary>
+        /// 图像对比度调整
+        /// </summary>
+        /// <param name="b">原始图</param>
+        /// <param name="degree">对比度[-100, 100]</param>
+        /// <returns></returns>
+        public static Bitmap SetContrast(Bitmap b, int degree)
+        {
+            if (b == null)
+            {
+                return null;
+            }
+
+            if (degree < -100) degree = -100;
+            if (degree > 100) degree = 100;
+
+            try
+            {
+
+                double pixel = 0;
+                double contrast = (100.0 + degree) / 100.0;
+                contrast *= contrast;
+                int width = b.Width;
+                int height = b.Height;
+                BitmapData data = b.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+                unsafe
+                {
+                    byte* p = (byte*)data.Scan0;
+                    int offset = data.Stride - width * 3;
+                    for (int y = 0; y < height; y++)
+                    {
+                        for (int x = 0; x < width; x++)
+                        {
+                            // 处理指定位置像素的对比度
+                            for (int i = 0; i < 3; i++)
+                            {
+                                pixel = ((p[i] / 255.0 - 0.5) * contrast + 0.5) * 255;
+                                if (pixel < 0) pixel = 0;
+                                if (pixel > 255) pixel = 255;
+                                p[i] = (byte)pixel;
+                            } // i
+                            p += 3;
+                        } // x
+                        p += offset;
+                    } // y
+                }
+                b.UnlockBits(data);
+                return b;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
